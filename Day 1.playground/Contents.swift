baed0250240fc562b4dd19bf463cf57f9cc347d4
Typo <typo@admins-iMac-5.local>
2017-07-11 15:17:29 +0400

    Initial commit

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+struct Student {
+    let name: String
+    let age: Int	
+}
+let vlad: Student = Student.init(name:"VLad",age:18)
+print(vlad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
